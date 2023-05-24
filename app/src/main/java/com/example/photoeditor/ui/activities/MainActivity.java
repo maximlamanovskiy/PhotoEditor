86ba08d9f0d54ab9e0b1eebe7744840624fa03d5
Maxim Lamanovsky <maxim.lamanovsky@7bits.it>
2023-05-24 11:38:18 +0600

    Fix asking for permissions and working with camera

--- app/src/main/java/com/example/photoeditor/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/photoeditor/ui/activities/MainActivity.java
@@ -9,7 +9,9 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -34,7 +36,8 @@ import static com.example.photoeditor.core.classes.Constants.TEMP_PICTURE_DIRECT
 @SuppressWarnings("unused")
 public class MainActivity extends AppCompatActivity {
 
-    @BindString(R.string.market_dev_name) String market_dev_name;
+    @BindString(R.string.market_dev_name)
+    String market_dev_name;
 
     private final static int COLLECTION_REQUEST = 2;
     private final static int GALLERY_REQUEST = 1;
@@ -45,31 +48,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        requestPermissions();
+        requestWorkingWithAllFilesAndroidRAndHigher();
+    }
+
+    void requestPermissions() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA,
+                },
+                PackageManager.PERMISSION_GRANTED
+        );
+    }
+
+    void requestWorkingWithAllFilesAndroidRAndHigher() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            intent.setData(Uri.fromParts("package", getPackageName(), null));
+            startActivity(intent);
+        }
     }
 
     @OnClick(R.id.collection)
-    void onCollectionClick(){
-        ActivityCompat.requestPermissions(this, new String[]{
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-        }, COLLECTION_REQUEST);
+    void onCollectionClick() {
+        startActivity(new Intent(this, CollectionActivity.class));
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
     @OnClick(R.id.camera)
-    void onCameraClick(){
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, CAMERA_REQUEST);
+    void onCameraClick() {
+        Uri cameraUri;
+        boolean success = true;
+        File storageDir = new File(TEMP_PICTURE_DIRECTORY);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs();
+        }
+
+        if (success) {
+            File file = new File(storageDir, "temp-original.jpg");
+            cameraUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", file);
+            cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, cameraUri);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
     }
 
     @OnClick(R.id.gallery)
-    void onGalleryClick(){
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, GALLERY_REQUEST);
+    void onGalleryClick() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.select_picture)), GALLERY_REQUEST);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        switch (requestCode){
+        switch (requestCode) {
 
             case CAMERA_REQUEST:
                 if (!(grantResults.length > 0
@@ -135,7 +177,9 @@ public class MainActivity extends AppCompatActivity {
                     boolean success = true;
                     File storageDir = new File(TEMP_PICTURE_DIRECTORY);
 
-                    if (!storageDir.exists()){success = storageDir.mkdirs();}
+                    if (!storageDir.exists()) {
+                        success = storageDir.mkdirs();
+                    }
 
                     if (success) {
                         File file = new File(storageDir, "temp-original.jpg");
